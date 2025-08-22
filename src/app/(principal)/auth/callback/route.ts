30c3dff7f5dbac9022fa415b3d7a50d3cd0074dc
Rodrigo Vega <rodrigovmdev10@gmail.com>
2025-08-21 22:59:40 -0600

    hotfix: Change the auth callback

--- src/app/(principal)/auth/callback/route.ts
+++ src/app/(principal)/auth/callback/route.ts
@@ -1,28 +1,24 @@
 import { NextResponse } from 'next/server'
 import { createClient } from '@/db/supabase/server'
+import { EnvConfig } from '@/shared/config/envs'
 
 export async function GET(request: Request) {
-  const { origin } = new URL(request.url)
   const code = new URL(request.url).searchParams.get('code')
-  const next = '/dashboard' // <--- Always redirect to dashboard
+  const next = '/dashboard'
 
   if (code) {
     const supabase = await createClient()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
 
     if (!error) {
-      const forwardedHost = request.headers.get('x-forwarded-host')
-      const isLocalEnv = process.env.NODE_ENV === 'development'
+      const appUrl =
+        process.env.NODE_ENV === 'development'
+          ? new URL(request.url).origin // localhost in dev
+          : EnvConfig().NEXT_PUBLIC_APP_URL // always your prod domain
 
-      if (isLocalEnv) {
-        return NextResponse.redirect(`${origin}${next}`)
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`)
-      } else {
-        return NextResponse.redirect(`${origin}${next}`)
-      }
+      return NextResponse.redirect(`${appUrl}${next}`)
     }
   }
 
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
+  return NextResponse.redirect(`${EnvConfig().NEXT_PUBLIC_APP_URL}/auth/auth-code-error`)
 }
